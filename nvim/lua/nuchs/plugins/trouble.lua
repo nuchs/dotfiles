04e56aae8e9ea81d83b8c8cbcc4b0fe316340ef2
nuchs <sjbrown@live.co.uk>
2023-12-14 15:42:59 +0000

    Put trouble display at top

--- nvim/lua/nuchs/plugins/trouble.lua
+++ nvim/lua/nuchs/plugins/trouble.lua
@@ -1,33 +1,36 @@
 local function trouble_toggle()
-	require("trouble").toggle()
+  require("trouble").toggle()
 end
 
 local function trouble_workspace()
-	require("trouble").open("workspace_diagnostics")
+  require("trouble").open("workspace_diagnostics")
 end
 local function trouble_document()
-	require("trouble").open("document_diagnostics")
+  require("trouble").open("document_diagnostics")
 end
 local function trouble_quickfix()
-	require("trouble").open("quickfix")
+  require("trouble").open("quickfix")
 end
 local function trouble_loclist()
-	require("trouble").open("loclist")
+  require("trouble").open("loclist")
 end
 local function trouble_references()
-	require("trouble").open("lsp_references")
+  require("trouble").open("lsp_references")
 end
 
 return {
-	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	keys = {
-		{ "<Leader>xx", trouble_toggle, desc = "Toggle trouble" },
-		{ "<Leader>xw", trouble_workspace, desc = "Trouble in the workspace" },
-		{ "<Leader>xd", trouble_document, desc = "Trouble in the document" },
-		{ "<Leader>xq", trouble_quickfix, desc = "Trouble quickfix list" },
-		{ "<Leader>xl", trouble_loclist, desc = "Trouble location list" },
-		{ "gR", trouble_references, desc = "Trouble references" },
-	},
-	opts = {},
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    { "<Leader>xx", trouble_toggle,     desc = "Toggle trouble" },
+    { "<Leader>xw", trouble_workspace,  desc = "Trouble in the workspace" },
+    { "<Leader>xd", trouble_document,   desc = "Trouble in the document" },
+    { "<Leader>xq", trouble_quickfix,   desc = "Trouble quickfix list" },
+    { "<Leader>xl", trouble_loclist,    desc = "Trouble location list" },
+    { "gR",         trouble_references, desc = "Trouble references" },
+  },
+  opts = {
+    position = "top",
+    height = 6,
+  },
 }
