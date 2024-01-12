958151f60ea503e3e54e8ed03bd3379b7bd01fa1
nuchs <sjbrown@live.co.uk>
2024-01-12 14:50:17 +0000

    Add keymaps for terminal

---
+++ nvim/lua/nuchs/plugins/toggleterm.lua
@@ -0,0 +1,21 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  config = function()
+    require("toggleterm").setup({
+      direction = "vertical",
+      size = 50,
+      start_in_insert = true,
+    })
+
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+    vim.keymap.set('t', '<C-\\>', '<CMD>exe v:count1 . "ToggleTerm name=\\"term" . v:count1 . "\\""<CR>', opts)
+    vim.keymap.set('', '<C-\\>', '<CMD>exe v:count1 . "ToggleTerm name=\\"term" . v:count1 . "\\""<CR>', opts)
+
+    -- Go specific terminal mappings, probably need a better place for this
+    vim.keymap.set('n', '<Leader>gb', '<CMD>exe v:count1 . "TermExec cmd=\\"go build .\\" name=\\"term1\\""<CR>', opts)
+    vim.keymap.set('n', '<Leader>gr', '<CMD>exe v:count1 . "TermExec cmd=\\"go run .\\" name=\\"term1\\""<CR>', opts)
+    vim.keymap.set('n', '<Leader>gt', '<CMD>exe v:count1 . "TermExec cmd=\\"go test ./...\\" name=\\"term1\\""<CR>', opts)
+  end
+}
