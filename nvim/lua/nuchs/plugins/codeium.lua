c01b18ef7cb9510ea72db69ba77246e0d58c172f
nuchs <sjbrown@live.co.uk>
2024-03-28 07:22:32 +0000

    Remove some unused stuff, explicitly specify sway outputs and add bindings to exit sway and open a codeium chat in nvim

--- nvim/lua/nuchs/plugins/codeium.lua
+++ nvim/lua/nuchs/plugins/codeium.lua
@@ -3,11 +3,12 @@ return {
   event = 'BufEnter',
   config = function()
     vim.g.codeium_disable_bindings = 1
-    vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+    vim.keymap.set('i', '<C-Tab>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
     vim.keymap.set('i', '<C-]>', function() return vim.fn['codeium#CycleCompletions'](1) end,
       { expr = true, silent = true })
     vim.keymap.set('i', '<C-[>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
       { expr = true, silent = true })
     vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+    vim.keymap.set('n', '<Leader>c', function() return vim.fn['codeium#Chat']() end, { expr = true, silent = true })
   end
 }
